e79da58e4d61cd98b27244cbbadef3d01e7196ea
Wxq <Wxq@123.cn>
2026-02-01 09:20:37 +0800

    树形组件的应用

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -4,8 +4,13 @@
     <div class="dashboard-text">name: {{ name }}</div>
     <!-- 使用svg图标组件
     设置icon-class属性来切换图标 -->
-    <svg-icon icon-class="eye"></svg-icon>
-    <svg-icon icon-class="wechat"></svg-icon>
+    <!-- <svg-icon icon-class="eye"></svg-icon>
+    <svg-icon icon-class="wechat"></svg-icon> -->
+
+    <el-tree :data="depts"
+             :props="defaultProps"
+             @node-click="handleNodeClick"
+             default-expand-all></el-tree>
   </div>
 </template>
 
@@ -15,6 +20,26 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      depts: [{
+        label: '一级',
+        children: [{
+          label: '二级1-1',
+          children: [{
+            label: '三级1-3'
+          }],
+        }]
+      },
+      { label: '二级' }
+      ],
+
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
   computed: {
     ...mapGetters([
       'name'
