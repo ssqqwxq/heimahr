e84f78dd8c2b5196b4f048828fa1b2c3442630a3
Wxq <Wxq@123.cn>
2026-02-19 20:17:43 +0800

    首页个人信息的显示

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,8 +4,12 @@ const getters = {
   device: state => state.app.device,
   token: state => state.user.token,
   avatar: state => state.user.userInfo.staffPhoto,//头像
-  name: state => state.user.userInfo.username,
+  name: state => state.user.userInfo.username,// 名称
   userId: state => state.user.userInfo.userId,
-  routes: state => state.user.routes
+  routes: state => state.user.routes,
+  company: state => state.user.userInfo.company,//公司名
+  departmentName: state => state.user.userInfo.departmentName //部门名
 }
 export default getters
+
+
